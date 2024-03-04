ea7e01f1d9087dd86b242a4c6c6b7374cbb11069
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-04 20:16:28 +0100

    add onResponse and onFailure

--- app/src/main/java/com/example/musicapp/ApiInterface.kt
+++ app/src/main/java/com/example/musicapp/ApiInterface.kt
@@ -11,5 +11,5 @@ interface ApiInterface {
     @GET("/search")
     @Headers("X-RapidAPI-Key: b585ddacc2mshf4aaa60df56a940p14b29fjsn6374a1559286",
         "X-RapidAPI-Hos: deezerdevs-deezer.p.rapidapi.com")
-    fun getData(@Query("q")query: String):Call<List<MyData>>
+    fun getData(@Query("q")query: String):Call<MyData>
 }
--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         val  data = retrofitBuild.getData("eminem")
 
-        data.enqueue(object :Callback<List<MyData>?>{
-            override fun onResponse(call: Call<List<MyData>?>, response: Response<List<MyData>?>) {
-                TODO("Not yet implemented")
+        data.enqueue(object :Callback<MyData?>{
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                val  dataList = response.body()
+                val  texView = findViewById<TextView>(R.id.helloText)
+                texView.text = dataList.toString()
+                Log.d("TAG: onResponse","onResponse: "+response.body())
             }
 
-            override fun onFailure(call: Call<List<MyData>?>, t: Throwable) {
-                TODO("Not yet implemented")
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("TAG: onResponse","onResponse: "+t.message)
             }
         })
     }
