a9f5de265576a55b068bcea77fe8bce9f5f359e7
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-04 19:52:12 +0100

    add class ApiInterface

---
+++ app/src/main/java/com/example/musicapp/ApiInterface.kt
@@ -0,0 +1,15 @@
+package com.example.musicapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+
+    @GET("/search")
+    @Headers("X-RapidAPI-Key: b585ddacc2mshf4aaa60df56a940p14b29fjsn6374a1559286",
+        "X-RapidAPI-Hos: deezerdevs-deezer.p.rapidapi.com")
+    fun getData(@Query("q")query: String):Call<List<MyData>>
+}
--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitBuild = Retrofit.Builder().baseUrl("https://deezerdevs-deezer.p.rapidapi.com")
+            .addConverterFactory( GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
     }
 }
