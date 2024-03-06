db4dca7d6b70d9624affa39bc896c1ab622a39c3
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-06 17:59:35 +0100

    update myAdapter

--- app/src/main/java/com/example/musicapp/MyAdapter.kt
+++ app/src/main/java/com/example/musicapp/MyAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.musicapp
 
 import android.app.Activity
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
@@ -37,7 +39,14 @@ class MyAdapter(val context: Activity, val dataList: List<Data>): RecyclerView.A
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentData = dataList[position]
+        val mediaPlayer  = MediaPlayer.create(context,currentData.preview.toUri())
         holder.title.text = currentData.title
         Picasso.get().load(currentData.album.cover).into(holder.image)
+        holder.play.setOnClickListener {
+            mediaPlayer.start()
+        }
+        holder.pause.setOnClickListener{
+            mediaPlayer.pause()
+        }
     }
 }
